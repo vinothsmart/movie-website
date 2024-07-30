f938dbb89f01b48fd05043792869434b254a7f3f
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-30 12:38:04 +0530

    Add lazy loading functionality to movie list images

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const arrows = document.querySelectorAll(".arrow");
 const movieList = document.querySelectorAll(".movie-list");
 
 arrows.forEach((arrow, i) => {
+  const itemNumber = movieList[i].querySelectorAll("img").length;
+  let clickCounter = 0;
   arrow.addEventListener("click", () => {
-    movieList[i].style.transform = `translateX(${
-      movieList[i].computedStyleMap().get("transform")[0].x.value - 300
-    }px)`;
+    clickCounter++;
+    if (itemNumber - clickCounter - 4 >= 0) {
+      movieList[i].style.transform = `translateX(${
+        movieList[i].computedStyleMap().get("transform")[0].x.value - 300
+      }px)`;
+    } else {
+      movieList[i].style.transform = `translateX(0)`;
+      clickCounter = 0;
+    }
   });
 });
